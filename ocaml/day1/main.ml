5088e0f9d275d330b1d11e5c6a339f8cd76f3558
James Davidson <jedavidson2000@gmail.com>
2022-12-01 23:41:03 +1100

    We do a little golfing (pt. 2)

--- ocaml/day1/main.ml
+++ ocaml/day1/main.ml
@@ -8,16 +8,14 @@ let rec parse_calories cs =
       let xs = parse_calories cs' in
       (List.hd_exn xs + int_of_string c) :: List.tl_exn xs
 
-let read_calories =
+let input =
   In_channel.read_lines "../inputs/day1.txt"
   |> parse_calories
   |> List.sort ~compare:Int.compare
   |> List.rev
 
 let () =
-  (* \n inserted because dune is ass *)
-  Printf.printf "\n";
-  let input = read_calories in
-  Printf.printf "Task 1: %d\n" (List.hd_exn input);
+  (* \n prepended because dune apparently doesn't do this for you ... ? *)
+  Printf.printf "\nTask 1: %d\n" (List.hd_exn input);
   Printf.printf "Task 2: %d\n"
     (List.take input 3 |> List.fold ~init:0 ~f:Int.( + ))
