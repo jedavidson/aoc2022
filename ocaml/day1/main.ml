65585bb4bf6f8fda249abe50ba05af3d24be9700
James Davidson <jedavidson2000@gmail.com>
2022-12-01 22:37:43 +1100

    Day 1 in OCaml

---
+++ ocaml/day1/main.ml
@@ -0,0 +1,25 @@
+open Core
+
+let rec parse_calories cs =
+  match cs with
+  | [] -> [ 0 ]
+  | "" :: cs' -> 0 :: parse_calories cs'
+  | c :: cs' ->
+      let xs = parse_calories cs' in
+      (List.hd_exn xs + int_of_string c) :: List.tl_exn xs
+
+let read_calories =
+  In_channel.read_lines "./../inputs/day1.txt"
+  |> parse_calories
+  |> List.sort ~compare:Int.compare
+  |> List.rev
+
+let most_calories_carried total_cals = List.hd_exn total_cals
+
+let calories_carried_by_top_3_elves (total_cals : int list) =
+  List.take total_cals 3 |> List.fold ~init:0 ~f:Int.( + )
+
+let () =
+  (* \n inserted because dune is ass *)
+  Printf.printf "\nTask 1: %d\n" (most_calories_carried read_calories);
+  Printf.printf "Task 2: %d\n" (calories_carried_by_top_3_elves read_calories)
