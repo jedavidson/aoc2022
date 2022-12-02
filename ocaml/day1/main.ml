c4d007d6d15ca4c08601d0706271e772e7a49e35
James Davidson <jedavidson2000@gmail.com>
2022-12-02 12:14:20 +1100

    Commit less camel crimes

--- ocaml/day1/main.ml
+++ ocaml/day1/main.ml
@@ -5,17 +5,14 @@ let rec parse_calories cs =
   | [] -> [ 0 ]
   | "" :: rest -> 0 :: parse_calories rest
   | cals :: rest ->
-      let rest = parse_calories rest in
-      (List.hd_exn rest + int_of_string cals) :: List.tl_exn rest
+      let rest' = parse_calories rest in
+      (List.hd_exn rest' + Int.of_string cals) :: List.tl_exn rest'
 
 let input =
   In_channel.read_lines "../inputs/day1.txt"
   |> parse_calories
-  |> List.sort ~compare:Int.compare
-  |> List.rev
+  |> List.sort ~compare:(Fn.flip Int.compare)
 
 let () =
-  (* \n prepended because dune apparently doesn't do this for you ... ? *)
-  Printf.printf "\nTask 1: %d\n" (List.hd_exn input);
-  Printf.printf "Task 2: %d\n"
-    (List.take input 3 |> List.fold ~init:0 ~f:Int.( + ))
+  printf "Task 1: %d\n" (List.hd_exn input);
+  printf "Task 2: %d\n" (List.take input 3 |> List.fold ~init:0 ~f:( + ))
