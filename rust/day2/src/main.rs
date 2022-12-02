0aa4368b225c61325cb3278aaa075c094ff7c7ea
James Davidson <jedavidson2000@gmail.com>
2022-12-02 17:03:26 +1100

    Add extra crate setup stuff

--- rust/day2/src/main.rs
+++ rust/day2/src/main.rs
@@ -10,29 +10,31 @@ fn read_rounds() -> IOResult<Vec<Round>> {
     Ok(BufReader::new(File::open(INPUT_PATH)?)
         .lines()
         .map(|line| {
-            line.expect("Lines a formatted properly")
+            line.expect("Lines are formatted properly")
                 .split_whitespace()
                 .map(Shape::from)
-                .collect::<Vec<Shape>>()
+                .collect::<Vec<_>>()
         })
         .map(|round_vec| (round_vec[0], round_vec[1]))
         .collect())
 }
 
-fn total_score(rounds: &Vec<Round>) -> Score {
-    rounds.iter().fold(0, |acc, &round| acc + get_score(round))
+fn total_score_faceoff(rounds: &Vec<Round>) -> Score {
+    rounds
+        .iter()
+        .fold(0, |score, &round| score + get_score(round))
 }
 
-fn total_score_with_intended_strategy(rounds: &Vec<Round>) -> Score {
+fn total_score_strategy(rounds: &Vec<Round>) -> Score {
     rounds
         .iter()
         .map(|&(them, you)| (them, Outcome::from(you)))
-        .fold(0, |acc, inst| acc + play_instruction(inst))
+        .fold(0, |score, inst| score + play_instruction(inst))
 }
 
 fn main() -> IOResult<()> {
     let rounds = read_rounds()?;
-    println!("Task 1: {}", total_score(&rounds));
-    println!("Task 1: {}", total_score_with_intended_strategy(&rounds));
+    println!("Task 1: {}", total_score_faceoff(&rounds));
+    println!("Task 2: {}", total_score_strategy(&rounds));
     Ok(())
 }
