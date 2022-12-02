32be4e182ea025aa6f386d481ec988cb5d941f2d
James Davidson <jedavidson2000@gmail.com>
2022-12-02 20:29:47 +1100

    Rewrite OCaml solution to use formulaic approach

--- ocaml/day2/main.ml
+++ ocaml/day2/main.ml
@@ -1,12 +1,24 @@
 open Core
-open Types
 
-let input = In_channel.read_lines "../inputs/day2.txt" |> List.map ~f:to_round
-let total_score rounds = List.map ~f:score rounds |> List.fold ~init:0 ~f:( + )
+let input_path = "../inputs/day2.txt"
 
-let total_score_with_strategy rounds =
-  List.map ~f:score_with_strategy rounds |> List.fold ~init:0 ~f:( + )
+let as_int c =
+  match c with
+  | 'A' | 'B' | 'C' -> Char.to_int c - Char.to_int 'A'
+  | 'X' | 'Y' | 'Z' -> Char.to_int c - Char.to_int 'X'
+  | _ -> failwith "Bad line entry"
+
+let to_round s =
+  match String.split s ~on:' ' with
+  | them :: us :: _ -> (as_int @@ String.get them 0, as_int @@ String.get us 0)
+  | _ -> failwith "Bad line format"
+
+(* With each shape mapped into Z_3, we can determine scores formulaically *)
+let score (them, us) = 1 + us + (3 * ((us - them + 1) % 3))
+let score_with_strategy (them, us) = (3 * us) + (1 + ((us + them - 1) % 3))
+let total_score ~f rounds = List.map ~f rounds |> List.fold ~init:0 ~f:( + )
 
 let () =
-  printf "Task 1: %d\n" @@ total_score input;
-  printf "Task 2: %d\n" @@ total_score_with_strategy input
+  let rounds = In_channel.read_lines input_path |> List.map ~f:to_round in
+  printf "Task 1: %d\n" @@ total_score rounds ~f:score;
+  printf "Task 2: %d\n" @@ total_score rounds ~f:score_with_strategy
