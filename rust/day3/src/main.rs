15d2f5b899fd95ed07150d07e720bc7ca7a4b601
James Davidson <jedavidson2000@gmail.com>
2022-12-03 17:59:57 +1100

    Improve day 3 Rust solution

--- rust/day3/src/main.rs
+++ rust/day3/src/main.rs
@@ -8,20 +8,24 @@ type Item = char;
 type Rucksack = String;
 type Priority = u32;
 
-fn read_input() -> IOResult<Vec<Rucksack>> {
+fn read_rucksacks() -> IOResult<Vec<Rucksack>> {
     Ok(BufReader::new(File::open(INPUT_PATH)?)
         .lines()
         .flat_map(|r| r)
-        .collect::<Vec<_>>())
+        .collect())
 }
 
-fn as_item_set(rucksack: &str) -> HashSet<Item> {
-    rucksack.chars().collect::<HashSet<_>>()
+fn as_char_set(s: &str) -> HashSet<Item> {
+    s.chars().collect()
 }
 
-macro_rules! intersection {
-    ($x:expr) => { as_item_set($x) };
-    ($x:expr, $($xs:expr),+) => { &as_item_set($x) & &(intersection!($($xs),+)) }
+macro_rules! string_intersection {
+    ($x:expr) => {
+        as_char_set($x)
+    };
+    ($x:expr, $($xs:expr),+) => {
+        &as_char_set($x) & &(string_intersection!($($xs),+))
+    }
 }
 
 fn get_priority(item: Item) -> Priority {
@@ -38,7 +42,7 @@ fn sum_common_item_priorities(rucksacks: &Vec<Rucksack>) -> Priority {
         .iter()
         .map(|r| {
             let (fst, snd) = r.split_at(r.len() / 2);
-            let common_item = intersection!(fst, snd)
+            let common_item = string_intersection!(fst, snd)
                 .iter()
                 .next()
                 .expect("At least one item is common to both compartments")
@@ -52,20 +56,22 @@ fn sum_badge_priorities(rucksacks: &Vec<Rucksack>) -> Priority {
     rucksacks
         .chunks(3)
         .into_iter()
-        .map(|chunk| {
-            let [fst, snd, thd] = chunk else { return 0; };
-            let common_item = intersection!(fst, snd, thd)
+        .map(|group| {
+            let [fst, snd, thd] = group else {
+                return 0;
+            };
+            let badge = string_intersection!(fst, snd, thd)
                 .iter()
                 .next()
                 .expect("Every elf in each group has one item in common")
                 .to_owned();
-            get_priority(common_item)
+            get_priority(badge)
         })
         .sum()
 }
 
 fn main() -> IOResult<()> {
-    let rucksacks = read_input()?;
+    let rucksacks = read_rucksacks()?;
     println!("Task 1: {:?}", sum_common_item_priorities(&rucksacks));
     println!("Task 2: {:?}", sum_badge_priorities(&rucksacks));
     Ok(())
