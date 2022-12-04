c405420f99bccb23bd45caa55f04e199264269fb
James Davidson <jedavidson2000@gmail.com>
2022-12-04 22:31:20 +1100

    Improved day 4 Rust solution

--- rust/day4/src/main.rs
+++ rust/day4/src/main.rs
@@ -4,40 +4,34 @@ use std::io::{BufRead, BufReader, Result as IOResult};
 const INPUT_PATH: &str = "../inputs/day4.txt";
 
 type Section = u32;
-type Assignment = (Section, Section);
-type Pair = (Assignment, Assignment);
+type Assignment = (Section, Section, Section, Section);
 
-fn as_pair(line: &str) -> Pair {
-    let mut pairs = line.split(',').map(|s| {
-        let mut sections = s.split('-').map(|n| n.parse::<Section>().unwrap());
-        (sections.next().unwrap(), sections.next().unwrap())
-    });
-    (pairs.next().unwrap(), pairs.next().unwrap())
+fn as_assignment(line: &str) -> Assignment {
+    let sections = line
+        .split(&[',', '-'])
+        .map(|s| s.parse::<Section>().unwrap())
+        .collect::<Vec<_>>();
+    (sections[0], sections[1], sections[2], sections[3])
 }
 
-fn read_pairs() -> IOResult<Vec<Pair>> {
+fn read_assignments() -> IOResult<Vec<Assignment>> {
     Ok(BufReader::new(File::open(INPUT_PATH)?)
         .lines()
-        .flat_map(|line| line)
-        .map(|s| as_pair(&s))
+        .map(|s| as_assignment(&s.unwrap()))
         .collect())
 }
 
-fn is_subsumed(((start1, end1), (start2, end2)): &&Pair) -> bool {
+fn is_subsumed((start1, end1, start2, end2): &&Assignment) -> bool {
     (start1 >= start2 && end1 <= end2) || (start1 <= start2 && end1 >= end2)
 }
 
-fn is_overlapping(((start1, end1), (start2, end2)): &&Pair) -> bool {
+fn is_overlapping((start1, end1, start2, end2): &&Assignment) -> bool {
     Section::max(*start1, *start2) <= Section::min(*end1, *end2)
 }
 
-fn count_satisfying_pairs(pairs: &Vec<Pair>, pred: impl FnMut(&&Pair) -> bool) -> usize {
-    pairs.iter().filter(pred).count()
-}
-
 fn main() -> IOResult<()> {
-    let pairs = read_pairs()?;
-    println!("Task 1: {}", count_satisfying_pairs(&pairs, is_subsumed));
-    println!("Task 2: {}", count_satisfying_pairs(&pairs, is_overlapping));
+    let assigns = read_assignments()?;
+    println!("Task 1: {}", assigns.iter().filter(is_subsumed).count());
+    println!("Task 2: {}", assigns.iter().filter(is_overlapping).count());
     Ok(())
 }
