98f205f6fc0cf605e3b85db16820af0d979d6b67
James Davidson <jedavidson2000@gmail.com>
2022-12-06 17:34:41 +1100

    Simplify day 6 OCaml solution

--- ocaml/day6/main.ml
+++ ocaml/day6/main.ml
@@ -3,20 +3,15 @@ open Core
 let datastream =
   In_channel.read_lines "../inputs/day6.txt" |> List.hd_exn |> String.to_list
 
+let is_marker = Fn.compose not (List.contains_dup ~compare:compare_char)
+
 let first_marker_pos data marker_size =
-  let is_marker marker =
-    List.length marker = marker_size
-    && not (List.contains_dup ~compare:compare_char marker)
-  in
-  let rec iter_markers xs pos =
-    match xs with
-    | c :: c' :: cs' ->
-        if List.length cs' >= marker_size - 2 then
-          if is_marker (c :: c' :: List.take cs' (marker_size - 2)) then
-            pos + marker_size
-          else iter_markers (c' :: cs') (pos + 1)
-        else -1
-    | _ -> -1
+  let rec iter_markers stream pos =
+    let marker = List.take stream marker_size in
+    if List.length marker = marker_size then
+      if is_marker marker then pos + marker_size
+      else iter_markers (List.tl_exn stream) (pos + 1)
+    else -1
   in
   iter_markers data 0
 
